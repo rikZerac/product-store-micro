085911d249a2495741328057fb4c8806184e5677
Riccardo Caretti <postarcr@gmail.com>
2022-07-30 17:09:12 +0200

    Initial project from Spring Boot CLI 2.7.2

---
+++ src/main/groovy/dev/riccardo/productsstore/DemoApplication.groovy
@@ -0,0 +1,13 @@
+package dev.riccardo.productsstore
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DemoApplication, args)
+	}
+
+}
